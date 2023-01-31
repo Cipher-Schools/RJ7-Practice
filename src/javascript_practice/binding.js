86da5235f69b04abbd6d14bb5c0dd2b4237277bf
A-phenomenal1 <niteshch200053@gmail.com>
2023-01-31 14:28:52 +0530

    event loop

--- src/javascript_practice/binding.js
+++ src/javascript_practice/binding.js
@@ -99,32 +99,32 @@
 
 // Callback
 
-function calcAdd(a, b, funcCall) {
-  let sum;
-  setTimeout(() => {
-    sum = a + b;
-    funcCall(sum);
-  });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-  console.log({ sum });
-}
+// function calcAdd(a, b, funcCall) {
+//   let sum;
+//   setTimeout(() => {
+//     sum = a + b;
+//     funcCall(sum);
+//   }, 100);
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+//   console.log({ sum });
+// }
 
-function displayResult(res) {
-  console.log("result: ", res);
-}
+// function displayResult(res) {
+//   console.log("result: ", res);
+// }
 
-calcAdd(3, 5, displayResult);
+// calcAdd(3, 5, displayResult);
 
 // function calcAdd(a, b) {
 //   let sum = a + b;
@@ -147,30 +147,133 @@ calcAdd(3, 5, displayResult);
 
 //Synchronous Call
 
+// async function fetchData() {
+//   //synchronous
+//   fetch("https://dummyjson.com/todos", { method: "GET" })
+//     .then((res) => {
+//       console.log("res: ", res);
+//       return res.json();
+//     })
+//     .then((data) => console.log("sync: ", data));
+
+//   //Promise:
+//   // let myPromise = new Promise((resolve, reject) => {
+//   //   let res = fetch("https://dummyjson.com/todos");
+//   //   resolve(res);
+//   // });
+
+//   // console.log({ myPromise });
+
+//   // myPromise.then((res) => res.json()).then((data) => console.log(data));
+
+//   //asynchronous
+//   let res = await fetch("https://dummyjson.com/todos");
+//   console.log("Hello");
+//   let data = await res.json();
+//   console.log("async", data);
+// }
+
+// fetchData();
+
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 3;
+  if (a == 4) {
+    resolve(a);
+  } else {
+    reject("Error: " + a);
+  }
+});
+
+p.then((res) => {
+  console.log({ res });
+}).catch((err) => {
+  console.log({ err });
+});
+
+console.log("hello...");
+
+//Callback Hell
+
+// function interval(time=1000){
+//   setTimeout(()=> {
+//     console.log("hello from SetTimeut")
+//   }, time)
+// }
+
+// for(var i = 0; i<100; i++){
+//   interval(i*1000);
+// }
+
+// 1+2+3.,...100 =  100(100+1)/2 =50*101s
+
+// function intervalPromise(time) {
+//   let p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("hello from Promise");
+//     }, time);
+//   });
+//   p2.then((res) => {
+//     console.log("success");
+//   }).catch((err) => {
+//     console.log(err);
+//   });
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   intervalPromise(i * 1000);
+// }
+
+let promise1 = new Promise((resolve, reject) => {
+  resolve("Hello from promise 1");
+  // reject("error from 1");
+});
+let promise2 = new Promise((resolve, reject) => {
+  // resolve("Hello from promise 2");
+  reject("error from 2");
+});
+let promise3 = new Promise((resolve, reject) => {
+  resolve("Hello from promise 3");
+});
+
+Promise.all([promise1, promise2, promise3])
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+// promise1.then(res=> {
+//   console.log(res)
+//   promise2.then(res=> {
+//     console.log(res)
+//     promise3.then()
+//   }).catch()
+// })
+
+let promise4 = new Promise((resolve, reject) => {
+  resolve("Hello from promise 4");
+});
+
+promise4.then((res) => console.log(res)).catch((err) => console.log(err));
+
+fetch("/")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("hello from 1");
+    fetch("/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("hello from 2");
+        fetch("/")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("hello from 3");
+          })
+          .catch((err) => console.log("error fromn 3"));
+      })
+      .catch((err) => console.log("error fromn 2"));
+  })
+  .catch((err) => console.log("error from 1"));
+
 async function fetchData() {
-  //synchronous
-  fetch("https://dummyjson.com/todos", { method: "GET" })
-    .then((res) => {
-      console.log("res: ", res);
-      return res.json();
-    })
-    .then((data) => console.log("sync: ", data));
-
-  //Promise:
-  // let myPromise = new Promise((resolve, reject) => {
-  //   let res = fetch("https://dummyjson.com/todos");
-  //   resolve(res);
-  // });
-
-  // console.log({ myPromise });
-
-  // myPromise.then((res) => res.json()).then((data) => console.log(data));
-
-  //asynchronous
-  let res = await fetch("https://dummyjson.com/todos");
-  console.log("Hello");
-  let data = await res.json();
-  console.log("async", data);
+  let rs = await fetch("/");
+  let result = await rs.json();
+  console.log(result);
 }
-
-fetchData();
---
+++ src/javascript_practice/eventloop.js
@@ -0,0 +1,36 @@
+// console.log("start");
+// let a = 5;
+// function func() {
+//   a = 10;
+//   console.log(a);
+// }
+
+// func();
+// console.log("end");
+
+// console.log("start");
+
+// setTimeout(() => {
+//   console.log("hello");
+// }, 100);
+
+// console.log("end");
+for (var i = 0; i < 10; i++) console.log("in for");
+
+console.log("start");
+
+setTimeout(() => {
+  console.log("hello");
+});
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// let btn;
+// btn.addEventListner('click', ()=> {
+
+// })
+
+console.log("end");
